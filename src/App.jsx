d260db55d366ca32efa48bb649618e170a21935f
Sandeep <sandeep.patel4488@gmail.com>
2025-07-09 12:32:44 +0530

    CSS updated, ARIA added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ import './App.css'
 const ProgressBar = ({ progress }) => {
   return (
     <div className="outer">
-      <div className="inner" style={{ width: `${progress}%` }}>
+      <div 
+        className="inner" 
+        style={{ width: `${progress}%` }}
+        role="progressbar"
+        aria-valuenow={progress}
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
         {progress}%
       </div>
     </div>
@@ -12,12 +19,14 @@ const ProgressBar = ({ progress }) => {
 }
 
 function App() {
-  const [count, setCount] = useState(40)
+  const [count, setCount] = useState(5)
 
   return (
     <>
       <h1>Progress Bar</h1>    
       <ProgressBar progress={count} />
+      <button onClick={() => setCount(count + 5)} className='button-style'>Increase</button>
+      <button onClick={() => setCount(count - 5)} className='button-style'>Decrease</button>
     </>
   )
 }
